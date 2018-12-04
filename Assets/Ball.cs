6a2c1b8675364a6bfcaf398df89c0b3b0c2d0e84
pwarg001 <pwarg001@gold.ac.uk>
2018-12-04 00:12:26 +0000

    Score&Hoops
    Scoring System and Hoops

---
+++ Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            case "RedHoop":
+                Score.UpdateScore(50);
+                break;
+            case "GreenHoop":
+                Score.UpdateScore(25);
+                break;
+            case "BlueHoop":
+                Score.UpdateScore(10);
+                break;
+        }
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    static Text scoreText;
+    static int currentScore = 0;
+
+	void Start () {
+        scoreText = GameObject.FindGameObjectWithTag("ScoreBoard").GetComponent<Text>();
+        UpdateScore(currentScore);
+	}
+
+    public static void UpdateScore(int addedValue)
+    {
+        currentScore += addedValue;
+        scoreText.text = "" + currentScore;
+    }
+}
