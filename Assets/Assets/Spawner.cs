4beb9ba2d9a5156693fa19c737ddbe56fe540631
Gergarious <greg.burt.360@gmail.com>
2018-12-11 09:34:28 +0000

    Mole Scripts
    Updated mole along with scripts and prefabs

---
+++ Assets/Assets/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject mole;
+    public float MinX = -6.4f;
+    public float MaxX = 5.32f;
+    public float MinZ = -11f;
+    public float MaxZ = 14f;
+
+    void Start(){
+   
+    }
+
+    void Update(){
+
+        float x = Random.Range(MinX, MaxX);
+        float y = -.4f;
+        float z = Random.Range(MinZ, MaxZ);
+
+        if (Time.fixedTime % 4 == 0)
+        {
+            Instantiate(mole, new Vector3(x, y, z), Quaternion.Euler(-90,0,0));
+        }
+    }
+       
+}
---
+++ Assets/Assets/jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class jump : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float height;
+
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+  
+    }
+}
